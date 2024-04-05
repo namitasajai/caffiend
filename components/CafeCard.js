0266d99672e26e0b71e2ddde15cbe0f263d9b744
Namita Sajai <sajai.namita@gmail.com>
2024-04-05 19:52:31 -0400

    finalized cafe card still need to finish cafe card with favorited

--- components/CafeCard.js
+++ components/CafeCard.js
@@ -1,24 +1,33 @@
-// CafeCard.js
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { ArrowElbowUpRight } from "phosphor-react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { ArrowElbowUpRight, Plus, Coffee, ForkKnife, WifiHigh } from "phosphor-react-native";
+
 
 const CafeCard = ({ cafe }) => {
   return (
     <View style={styles.card}>
-      <View style={styles.content}>
+      <Image source={{ uri: cafe.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
         <Text style={styles.name}>{cafe.name}</Text>
         <View style={styles.subheading}>
           <Text style={styles.distance}>{cafe.distance} miles</Text>
           <TouchableOpacity style={styles.directionsButton}>
-            <ArrowElbowUpRight size={16} color="#fff" weight="regular" />
+            <ArrowElbowUpRight size={16} color="#FFFFFF" weight="bold" />
             <Text style={styles.directionsText}>Directions</Text>
           </TouchableOpacity>
         </View>
         <TouchableOpacity style={styles.offeringsButton}>
-          <Text style={styles.offeringsText}>Offerings +</Text>
+          <Text style={styles.offeringsText}>Offerings</Text>
+          <Plus size={16} color="#666666" />
         </TouchableOpacity>
-        <Text style={styles.rating}>{cafe.rating}</Text>
+        <View style={styles.icons}>
+          <Coffee size={16} color="#333333" />
+          <ForkKnife size={16} color="#333333" />
+          <WifiHigh size={16} color="#333333" />
+        </View>
+        <View style={styles.ratingContainer}>
+          <Text style={styles.rating}>{cafe.rating}</Text>
+        </View>
       </View>
     </View>
   );
@@ -29,58 +38,91 @@ const styles = StyleSheet.create({
     backgroundColor: "#F0E6D0",
     borderRadius: 20,
     padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 0,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 16,
+  },
+  details: {
+    flex: 1,
+    alignContent: 'top', 
   },
   subheading: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    justifyContent: "left",
     gap: 8, 
-  },
-  content: {
-    alignItems: "flex-start",
+    alignItems: "center",
   },
   name: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 16,
+    fontFamily: "Inter_500Medium",
+    color: '#333333',
+    marginBottom: 4,
   },
   distance: {
-    fontSize: 16,
-    color: "#666",
-    marginTop: 4,
+    fontSize: 12,
+    fontFamily: "Inter_400Regular",
+    color: "#666666",
   },
   directionsButton: {
     flexDirection: "row",
-    marginTop: 8,
     backgroundColor: "#7689AB",
     paddingHorizontal: 8,
     paddingVertical: 4,
     borderRadius: 20,
-    gap: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   directionsText: {
     color: "#fff",
-    fontSize: 16,
-    textAlign: "center",
+    fontSize: 12,
+    fontFamily: "Inter_400Regular",
+    marginLeft: 4,
   },
   offeringsButton: {
-    marginTop: 8,
+    flexDirection: "row",
+    justifyContent: 'space-between',
     backgroundColor: "#fff",
-    paddingHorizontal: 32,
+    fontFamily: "Inter_400Regular",
+    paddingHorizontal: 16,
     paddingVertical: 4,
     borderRadius: 20,
+    marginTop: 4,
+    marginBottom: 4,
+    alignItems: 'center',
+    width: 150, 
   },
   offeringsText: {
-    fontSize: 16,
+    fontSize: 12,
     color: "#666666",
-    textAlign: "center",
+  },
+  icons: {
+    flexDirection: "row",
+    gap: 8,
+    marginTop: 4,
+  },
+  ratingContainer: {
+    backgroundColor: "#4F1C11",
+    width: 40,
+    height: 40,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 0.5,
+    bottom: 0.5,
   },
   rating: {
     fontSize: 16,
     fontWeight: "bold",
-    color: "#444",
-    marginTop: 8,
+    fontFamily: "Inter_700Bold",
+    color: "#E58D23",
   },
 });
 
--- components/Header.js
+++ components/Header.js
@@ -14,10 +14,10 @@ const Header = () => {
   return (
     <View style={styles.container}>
       <View style={styles.statusBarPlaceholder} />
-      <Text style={styles.timeText}>9:41</Text>
+      <Text style={styles.title}>Location</Text>
 
       <View style={styles.locationContainer}>
-        <MapPin size={16} color="#333" weight="bold" />
+        <MapPin size={18} color="#333" weight="bold" />
         <Text style={styles.locationText}>New York, NY, USA</Text>
       </View>
 
@@ -38,28 +38,29 @@ const styles = StyleSheet.create({
     borderRadius: 38,
     height: screenHeight / 5, // Set the height to 1/5th of the screen height
     justifyContent: "space-around", // Distribute children evenly
-    paddingTop: 20, // Increase the padding to give some space at the top
-    paddingBottom: 20, // Similarly, adjust the bottom padding
-    paddingHorizontal: 16, // Set horizontal padding to create space on the sides
+    paddingTop: 40, // Increase the padding to give some space at the top
+    paddingBottom: 30, // Similarly, adjust the bottom padding
+    paddingHorizontal: 30, // Set horizontal padding to create space on the sides
   },
   statusBarPlaceholder: {
-    height: 20, // Adjust if needed for the specific iPhone model you are targeting
+    height: 40, // Adjust if needed for the specific iPhone model you are targeting
   },
-  timeText: {
-    fontSize: 17,
-    fontWeight: "600",
-    color: "black",
-    alignSelf: "flex-start",
-    marginTop: -28, // Adjust as needed for your layout
+  title: {
+    fontSize: 14,
+    color: "#333333",
+    fontWeight: "light",
+    marginTop: 10, 
+    marginBottom: 8, 
   },
   locationContainer: {
     flexDirection: "row",
     alignItems: "center",
-    marginTop: 10, // Adjust as needed
+    marginTop: 10, 
+    marginBottom: 15, 
   },
   locationText: {
     marginLeft: 8,
-    fontSize: 15,
+    fontSize: 16,
     color: "#333333",
     fontWeight: "400",
   },
